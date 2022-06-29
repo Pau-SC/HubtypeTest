1818e906366c15cc7ff060e13a86c6a8ec366156
Pau <105737277+Pau-SC@users.noreply.github.com>
2022-06-29 19:42:38 +0200

    Style changes

--- src/webchat/index.js
+++ src/webchat/index.js
@@ -1,1 +1,49 @@
-export const webchat = {}
+import gotLogo from '../assets/got-Logo.png'
+import gotLauncher from '../assets/got-Launcher.png'
+
+export const webchat = {
+  theme: {
+      style:{
+        backgroundColor: 'grey',
+      },
+      userMessageStyle: {
+          backgroundColor: '#404040',
+          borderRadius: '65px',
+      },
+      userInput: {
+          style: {
+            background: '#000000',
+          },
+          box: {
+            style: {
+              border: '2px solid #ABABAB',
+              color: '#ABABAB',
+              align: 'center',
+              wrap: 'nowrap',
+            },
+          }
+
+      },
+      triggerButton:{
+          style:{
+            backgroundColor: '#000000',
+          },
+          image: gotLauncher,
+      },
+      brand: {
+          image: gotLogo,
+        },
+      header: {
+        title: 'Winter is coming...',
+        subtitle: 'GOT',
+        image: gotLogo,
+        style: {
+          height: 40,
+          background: 'linear-gradient(to left, #ABABAB,#000000  65%)',
+        },
+      intro: {
+          image: gotLauncher,
+        },
+      },
+  },
+}
